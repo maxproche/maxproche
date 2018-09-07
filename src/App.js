9dc35bb40ee16818114eb84f9ebf0192a72c86fd
Max Roche <maxwell@thinkbiosolution.com>
2018-09-07 15:39:47 -0400

    Absolute imports are now working

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+// Layouts
+import DefaultLayout from 'layouts/default/DefaultLayout';
+
+class App extends Component {
+	render() {
+		return <DefaultLayout />;
+	}
+}
+
+export default App;
---
+++ src/layouts/default/DefaultLayout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class DefaultLayout extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Default Layout working correctly</p>
+			</div>
+		);
+	}
+}
+
+export default DefaultLayout;
