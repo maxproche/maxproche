a6743c6b63db95014baf07a184d8991c013e11bd
Max Roche <maxwell@thinkbiosolution.com>
2018-10-10 17:49:29 -0400

    Added card components and have the high school poriton working

---
+++ src/components/common/cards/horizontal-card/HorizontalCard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// Styling
+import styles from './HorizontalCard.css';
+
+const HorizontalCard = (props) => {
+	const { imageSrc, title, description, date, details, extra, link } = props;
+	debugger;
+	return (
+		<a className={styles.anchor} href={link} target="_blank">
+			<div className={styles.cardContainer}>
+				<div className={styles.image}>
+					<img src={imageSrc} />
+				</div>
+				<div className={styles.cardInfo}>
+					<div className={styles.header}>
+						<div className={styles.title}>
+							<div>{title}</div>
+							<div className={styles.description}>{description}</div>
+						</div>
+						<div className={styles.date}>{date}</div>
+					</div>
+
+					<div className={styles.details}>
+						<ul>{details.map((detail) => <li>{detail}</li>)}</ul>
+					</div>
+
+					<div className={styles.extra}>{extra}</div>
+				</div>
+			</div>
+		</a>
+	);
+};
+
+HorizontalCard.propTypes = {
+	imageSrc: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	date: PropTypes.string.isRequired,
+	details: PropTypes.array.isRequired,
+	extra: PropTypes.string.isRequired,
+	link: PropTypes.string
+};
+
+export default HorizontalCard;
